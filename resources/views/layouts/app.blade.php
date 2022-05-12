81abe3eb0ca386c07faf32de53e4fd599b386724
PRONOVA-WEB <adiaz@pronova.cl>
2022-05-12 15:49:23 -0400

    cambio de title header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ settings('site.title') }} | @yield('title')</title>
+    <title>{{ settings('site.title') }} | Sanatorio Alemán</title>
     <meta content="Pronova" name="author" />
     <!-- Scripts -->
 
