a3703ba3156051ab5ae2de986b9c9d6b861a8de8
PRONOVA-WEB <adiaz@pronova.cl>
2022-05-12 14:19:47 -0400

    config livewire testing

--- config/livewire.php
+++ config/livewire.php
@@ -54,8 +54,8 @@ return [
     |
     */
 
-    // 'asset_url' => env('APP_URL'),
-    'asset_url' => null,
+    'asset_url' => env('APP_URL'),
+    //'asset_url' => null,
 
     /*
     |--------------------------------------------------------------------------
@@ -69,8 +69,8 @@ return [
     |
     */
 
-    // 'app_url' => env('APP_URL'),
-    'app_url' => null,
+    'app_url' => env('APP_URL'),
+    //'app_url' => null,
 
     /*
     |--------------------------------------------------------------------------
